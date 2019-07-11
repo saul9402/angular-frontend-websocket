5201b9a264ee605817ea7e737d9e4fb218d33f6b
Saul Avila <saul_harry_@hotmail.com>
2019-07-11 10:13:20 -0500

    se implementa el envio y recepcion de mensajes

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,8 @@ export class ChatComponent implements OnInit {
 
   private client: Client;
   conectado: boolean = false;
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
 
 
   constructor() { }
@@ -29,6 +32,23 @@ export class ChatComponent implements OnInit {
       //el objeto frame contiene toda la información de la conexión con el broker
       console.log("Conectados: " + this.client.connected + " : " + frame);
       this.conectado = true;
+
+      /* Con esto te subscribes a los eventos del broker; 
+        el primer parametro es el nombre del evento a escuchar,
+        el segundo es una funcion que permite manejar cuando llega
+        un nuevo mensaje al broker.
+      */
+      this.client.subscribe('/chat/mensaje', e => {
+        /*este es el mensaje que alguien escribió, que llegó al broker
+        que el broker emitió a los subscritores y que llegó por ese medio
+        hasta este método.
+        */
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+
+      });
     }
 
     this.client.onDisconnect = (frame) => {
@@ -47,7 +67,17 @@ export class ChatComponent implements OnInit {
 
   desconectar(): void {
     this.client.deactivate();
+  }
 
+  enviarMensaje(): void {
+    /*con este metodo publicas un evento en el broker
+    como primer argumento se envia un objeto que llevará el destino
+    */
+    this.client.publish({
+      destination: "/app/mensaje",
+      body: JSON.stringify(this.mensaje)
+    });
+    this.mensaje.texto = '';
   }
 
 }
